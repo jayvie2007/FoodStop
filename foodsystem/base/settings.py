9ec63238601cb7fc520da278a12c0db21118b113
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-03-29 16:54:30 +0800

    added food, drink, side models

--- foodsystem/base/settings.py
+++ foodsystem/base/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'web_api',
+    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- foodsystem/web_api/models.py
+++ foodsystem/web_api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class FoodList(models.Model):
+    uid = models.CharField(max_length=8)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " " + self.price
+    
+class DrinkList(models.Model):
+    uid = models.CharField(max_length=8)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " " + self.price
+
+class Sidelist(models.Model):
+    uid = models.CharField(max_length=8)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " " + self.price
