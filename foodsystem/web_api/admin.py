8e2dd94e50b99918c3356325f0a6e7b7d2b259f0
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-03-29 16:57:23 +0800

    Added Superuser

--- foodsystem/web_api/admin.py
+++ foodsystem/web_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import FoodList, DrinkList, Sidelist
 
 # Register your models here.
+admin.site.register(FoodList)
+admin.site.register(DrinkList)
+admin.site.register(Sidelist)
+
+admin.site.site_header = ("Food Database")
